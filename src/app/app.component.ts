82de9572ce721cbbe5f16a6593952bc8b63d04e2
Willian Soares <dasilva.williansoares@gmail.com>
2020-12-18 16:20:18 -0300

    My img app angular with bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-img-app';
+  title = 'my img app';
+  description = 'teste';
+  url = 'https://dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2020/07/starfox-950x664.jpg';
 }
