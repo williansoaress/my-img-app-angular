8baa5c2cd657e7dfe2f221750f2b7c2ab3a5160e
Willian Soares <dasilva.williansoares@gmail.com>
2020-12-19 11:57:00 -0300

    Creating a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my img app';
-  description = 'teste';
-  url = 'https://dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2020/07/starfox-950x664.jpg';
+
+    photos = [
+      {
+        url: 'https://dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2020/07/starfox-950x664.jpg', 
+        description: 'Star fox'
+      },
+      {
+        url: 'https://dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2020/07/starfox-950x664.jpg', 
+        description: 'Star fox'
+      }
+    ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
